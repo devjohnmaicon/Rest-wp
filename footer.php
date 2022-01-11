739ffe5b89c95bcdad49455ee321c494c96c0dc6
John Maicon <johnmaiconnp@hotmail.com>
2022-01-11 12:54:42 -0300

    create template

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <nav class="nav-footer">
+        <ul>
+            <li class="current_page_item"><a href="/">Menu</a></li>
+            <li><a href="sobre">Sobre</a></li>
+            <li><a href="contato">Contato</a></li>
+        </ul>
+    </nav>
+
+    <p>Rest © 2015. Alguns direitos reservados.</p>
+</footer>
+
+<!-- FOOTER WP -->
+<?php wp_footer(  ) ?>
+<!-- FECHA FOOTER WP -->
+
+</body>
+
+</html>
